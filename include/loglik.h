f11365f80243e4b59a0151bb0e7c7f00a2a316de
hyuhngminkim <hminkim@kaist.ac.kr>
2023-05-31 00:55:40 +0900

    Rename src to include

---
+++ include/loglik.h
@@ -0,0 +1,41 @@
+#ifndef CNMLCD_LOGLIK_H_
+#define CNMLCD_LOGLIK_H_
+
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+
+#include "lcd.h"
+#include "utils.h"
+
+
+namespace cnmlcd {
+  
+// x must be a sorted array
+VectorT<mpf> dlcd(LCD& lcd, VectorT<mpf>& x, bool islog = false) {
+  long int x_cols = x.cols();
+  long int k_cols = lcd.knots.cols() - 1;
+  VectorT<mpf> logd(x_cols);
+
+  int j = 0;
+  for (int i = 0; i < x_cols; ++i) {
+    while (j < k_cols && lcd.knots(j + 1) <= x(i)) ++j;
+    logd(i) = lcd.intercept(j) + lcd.slope(j) * (x(i)) - log(lcd.C); //cast<double, T>
+  }
+  if (islog) {
+    return logd;
+  } else {
+    return logd.array().exp();
+  }
+}
+
+void loglik(LCD& lcd, VectorT<mpf> &x, const VectorT<int>& w) {
+  VectorT<mpf> r = dlcd(lcd, x, true);
+  r = r * w;
+  lcd.ll = r.sum();
+  return;
+}
+
+}         // namespace cnmlcd
+
+#endif    // CNMLCD_LOGLIK_H_
