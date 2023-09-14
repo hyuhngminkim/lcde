0d26432b809b76438f8026dcd24e84f60a010b28
hyuhngminkim <hminkim@kaist.ac.kr>
2023-09-14 21:16:59 +0900

    Fix minor bug and suboptimal code

--- include/lcde/loglik.h
+++ include/lcde/loglik.h
@@ -14,12 +14,13 @@ namespace lcde {
 // x must be a sorted array
 VectorT<mpf> dlcd(LCD& lcd, VectorT<mpf>& x, bool islog = false) {
   long int x_cols = x.cols();
-  long int k_cols = lcd.knots.cols();
+  long int k_cols = lcd.knots.cols() - 1;
   VectorT<mpf> logd(x_cols);
 
   int j = 0;
   for (int i = 0; i < x_cols; ++i) {
-    while (j < k_cols && lcd.knots(j + 1) <= x(i)) ++j;
+    // while (j < k_cols && lcd.knots(j + 1) <= x(i)) ++j;
+    if (j < k_cols && lcd.knots(j + 1) == x(i)) ++j;
     logd(i) = lcd.intercept(j) + lcd.slope(j) * (x(i)) - log(lcd.C); 
   }
   if (islog) {
